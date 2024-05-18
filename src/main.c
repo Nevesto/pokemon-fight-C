fa844a05b851867aa6097a6d34880f630173ada2
Nevesto <guilhermejcarlucci@hotmail.com>
2024-05-18 18:19:54 -0300

    fix pokemons size, background & font

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-#include "raylib.h"
+#include <raylib.h>
 #include <stdio.h>
 
 #define POKEMON_COUNT 4
@@ -10,6 +10,12 @@
 
 Image pokemonImages[POKEMON_COUNT];
 Texture2D pokemonTextures[POKEMON_COUNT];
+Font font;
+Image backgroundImage;
+Texture2D backgroundTexture;
+
+
+
 typedef enum {
     POKEMON_BULBASAUR,
     POKEMON_CHARMANDER,
@@ -27,28 +33,27 @@ PokemonButton pokemonButtons[POKEMON_COUNT];
 #define FILENAME_SIZE 100
 
 void InitPokemonButtons() {
-    int buttonX = (SCREEN_WIDTH - BUTTON_WIDTH * 2 - BUTTON_SPACING) / 2;
-    int buttonY = (SCREEN_HEIGHT / 2) - (BUTTON_HEIGHT + BUTTON_SPACING) * 2;
-
-    
+    int totalButtonsWidth = BUTTON_WIDTH * POKEMON_COUNT + BUTTON_SPACING * (POKEMON_COUNT - 1);
+    int buttonX = (SCREEN_WIDTH - totalButtonsWidth) / 2;
+    int buttonY = (SCREEN_HEIGHT / 2) - (BUTTON_HEIGHT + BUTTON_SPACING) * 2;    
 
     for (int i = 0; i < POKEMON_COUNT; i++) {
         char filename[FILENAME_SIZE];
         snprintf(filename, sizeof(filename), "../resources/menu/pokemon_%d.png", i + 1);
         pokemonImages[i] = LoadImage(filename);
+        ImageResize(&pokemonImages[i], 200, 200);
         pokemonTextures[i] = LoadTextureFromImage(pokemonImages[i]);
-        pokemonTextures[i] = LoadTextureFromImage(pokemonImages[i]);  // Use the original image without resizing
 
         pokemonButtons[i] = (PokemonButton){
             .texture = pokemonTextures[i],
-            .rec = (Rectangle){
+            .rec = (Rectangle) {
                 buttonX,
                 buttonY,
-                BUTTON_WIDTH,
-                BUTTON_HEIGHT
+                200,
+                200
             }
         };
-        buttonX += BUTTON_WIDTH + BUTTON_SPACING;
+        buttonX += pokemonImages[i].width + BUTTON_SPACING;
     }
 }
 
@@ -66,10 +71,28 @@ void UpdatePokemonMenu() {
     }
 }
 
+void DrawTextWithShadow(const char *text, int posX, int posY, int fontSize, Color color, Color shadowColor, int offsetX, int offsetY) {
+    // Desenhar a sombra
+    DrawText(text, posX + offsetX, posY + offsetY, fontSize, shadowColor);
+    
+    // Desenhar o texto principal
+    DrawText(text, posX, posY, fontSize, color);
+}
+
+
+
 void DrawPokemonMenu() {
     BeginDrawing();
     ClearBackground(RAYWHITE);
-    DrawText("Escolha seu Pokémon:", (SCREEN_WIDTH - MeasureText("Escolha seu Pokémon:", 20)) / 2, 50, 20, BLACK);
+
+    float scaleX = (float)SCREEN_WIDTH / backgroundTexture.width;
+    float scaleY = (float)SCREEN_HEIGHT / backgroundTexture.height;
+
+
+    DrawTexturePro(backgroundTexture, (Rectangle){ 0.0f, 0.0f, (float)backgroundTexture.width, (float)backgroundTexture.height },
+               (Rectangle){ 0.0f, 0.0f, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT }, (Vector2){ 0.0f, 0.0f }, 0.0f, WHITE);
+
+    DrawTextWithShadow("Escolha seu Pokémon:", (SCREEN_WIDTH - MeasureText("Escolha seu Pokémon:", 40)) / 2, 250, 40, WHITE, DARKGRAY, 2, 2);
 
     for (int i = 0; i < POKEMON_COUNT; i++) {
         if (selectedPokemon == i) {
@@ -77,18 +100,27 @@ void DrawPokemonMenu() {
         }
         DrawTexture(pokemonButtons[i].texture, pokemonButtons[i].rec.x, pokemonButtons[i].rec.y, WHITE);
     }
-    
+
     EndDrawing();
 }
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Pokemon Chooser");
+    LoadFont("../resources/Minecraft.ttf");
+    backgroundImage = LoadImage("../resources/background.png");
+    backgroundTexture = LoadTextureFromImage(backgroundImage);
     InitPokemonButtons();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         UpdatePokemonMenu();
         DrawPokemonMenu();
+    }
+
+    // Descarregar recursos
+    UnloadFont(font);
+    for (int i = 0; i < POKEMON_COUNT; i++) {
+        UnloadImage(pokemonImages[i]);
+        UnloadTexture(pokemonTextures[i]);
     }
 
     CloseWindow();
